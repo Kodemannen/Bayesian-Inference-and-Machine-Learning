a8fee4c53ddf093f95df5c134722e8aa85c6d211
kodemannen <samuelknudsen@hotmail.com>
2019-10-23 13:41:53 +0200

    asd

--- make_slides.sh
+++ make_slides.sh
@@ -1,1 +1,2 @@
-jupyter nbconvert slides_version.ipynb --to slides #--post serve
+#jupyter nbconvert slides_version.ipynb --to slides #--post serve
+jupyter nbconvert slides_version.ipynb --to markdown #--post serve
